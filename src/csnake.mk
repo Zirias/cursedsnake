af173cb2a5aa1820935333c708d3f1ca052e0e1b
Felix Palmen <felix@palmen-it.de>
2015-07-19 01:06:43 +0200

    link pdcurses for windows

--- src/csnake.mk
+++ src/csnake.mk
@@ -5,6 +5,7 @@ csnake_SOURCES:= csnake.c utils.c game.c board.c snake.c screen.c
 csnake_PLATFORMSOURCES:= utils.c
 csnake_LDFLAGS:= -pthread
 csnake_posix_LDFLAGS:= -lcurses
+csnake_win32_LDFLAGS:= -lpdcurses
 
 $(eval $(BINRULES))
 
