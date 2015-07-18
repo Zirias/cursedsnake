eac903d3e7dbf0eaddacb00f65b33402334ada85
Felix Palmen <felix@palmen-it.de>
2015-07-18 19:25:42 +0200

    curses screen should be ok for all platforms

--- src/csnake.mk
+++ src/csnake.mk
@@ -1,8 +1,7 @@
 P:= src
 T:= csnake
 
-csnake_SOURCES:= csnake.c board.c
-csnake_PLATFORMSOURCES:= screen.c
+csnake_SOURCES:= csnake.c board.c screen.c
 csnake_posix_LDFLAGS:= -lcurses
 
 $(eval $(BINRULES))
--- src/screen.c
+++ src/screen.c
@@ -1,4 +1,4 @@
-#include "../../screen.h"
+#include "screen.h"
 
 #include <stdlib.h>
 #include <curses.h>
