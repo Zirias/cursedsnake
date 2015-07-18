cdace62ca60c5ca1f2990f0a5ebe1ec73c768b21
Felix Palmen <felix@palmen-it.de>
2015-07-18 15:27:57 +0200

    formatting

--- src/csnake.mk
+++ src/csnake.mk
@@ -2,8 +2,8 @@ P:= src
 T:= csnake
 
 csnake_SOURCES:= csnake.c board.c
-csnake_PLATFORMSOURCES := screen.c
-csnake_posix_LDFLAGS := -lcurses
+csnake_PLATFORMSOURCES:= screen.c
+csnake_posix_LDFLAGS:= -lcurses
 
 $(eval $(BINRULES))
 
