5f61461b2eb8b2e5eeff57a0b3aae83162738778
Felix Palmen <felix@palmen-it.de>
2015-07-23 16:19:59 +0200

    draft code

--- src/centity.c
+++ src/centity.c
@@ -5,41 +5,48 @@
 #define LIST_SIZE 32
 #define DICT_BITS 6
 
-typedef enum
-{
-    CE_STRING,
-    CE_LIST,
-    CE_DICT
-} CeType;
+typedef struct cpertor Cpertor;
+
+Cpertor *cpertor_createRoot(const char *name);
+Cpertor *cpertor_createListEntry(Cpertor *parent, size_t index);
+Cpertor *cpertor_createDictEntry(
+	Cpertor *parent, size_t keylen, const void *key);
 
 struct centity
 {
     CeType type;
+    Cpertor *ptor;
 };
 
 typedef struct
 {
     CeType type;
+    Cpertor *ptor;
     char content[1];
 } CeString;
 
 typedef struct
 {
     CeType type;
+    Cpertor *ptor;
+    size_t entries;
+    size_t size;
     Centity *memb[1];
 } CeList;
 
-typedef struct
+typedef struct ceDictEntry CeDictEntry;
+struct ceDictEntry
 {
-    Centity *next;
+    CeDictEntry *next;
     Centity *ent;
     size_t keylen;
     char key[1];
-} CeDictEntry;
+};
 
 typedef struct
 {
     CeType type;
+    Cpertor *ptor;
     unsigned int hashmask;
     CeDictEntry *memb[1];
 } CeDict;
@@ -60,6 +67,8 @@ centity_createSizedList(size_t initialSize)
     CeList *self = malloc(sizeof(CeList) +
 	    (initialSize - 1U) * sizeof(Centity *));
     self->type = CE_LIST;
+    self->size = initialSize;
+    self->entries = 0U;
     memset(&(self->memb), 0, initialSize * sizeof(Centity *));
     return (Centity *)self;
 }
@@ -85,5 +94,65 @@ Centity *
 centity_createDict(void)
 {
     return centity_createBitsDict(DICT_BITS);
+}
+
+static void
+ceDictEntry_destroy(CeDictEntry *self)
+{
+    if (!self) return;
+
+    free(self->key);
+    centity_destroy(self->ent);
+    ceDictEntry_destroy(self->next);
+    free(self);
+}
+
+void
+centity_destroy(Centity *self)
+{
+    if (!self) return;
+
+    switch (self->type)
+    {
+	case CE_STRING:
+	    free(self);
+	    break;
+
+	case CE_LIST:
+	    {
+		CeList *list = (CeList *)self;
+		size_t i;
+
+		for (i = 0; i < list->entries; ++i)
+		{
+		    centity_destroy(list->memb[i]);
+		}
+	    }
+	    break;
+	
+	case CE_DICT:
+	    {
+		CeDict *dict = (CeDict *)self;
+		size_t i;
+
+		for (i = 0; i <= dict->hashmask; ++i)
+		{
+		    ceDictEntry_destroy(dict->memb[i]);
+		}
+	    }
+	    break;
+    }
+    free(self);
+}
+
+const char *
+centity_string(const Centity *self)
+{
+    if (self->type == CE_STRING)
+    {
+	const CeString *string = (const CeString *)self;
+	return &(string->content[0]);
+    }
+    return 0;
 }
 
--- src/centity.h
+++ src/centity.h
@@ -3,6 +3,13 @@
 
 #include "common.h"
 
+typedef enum
+{
+    CE_STRING,
+    CE_LIST,
+    CE_DICT
+} CeType;
+
 struct centity;
 typedef struct centity Centity;
 
@@ -11,5 +18,6 @@ Centity *centity_createSizedList(size_t initialSize);
 Centity *centity_createList(void);
 Centity *centity_ceateBitsDict(unsigned int hashbits);
 Centity *centity_createDict(void);
+void centity_destroy(Centity *self);
 
 #endif
