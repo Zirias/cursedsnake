de06761d593d9abfbc45b7a6f31c95e6e9fd0dc0
Felix Palmen <felix@palmen-it.de>
2015-07-23 09:15:23 +0200

    use usigned literals where indicated

--- src/centity.c
+++ src/centity.c
@@ -48,9 +48,9 @@ Centity *
 centity_createString(const char *string)
 {
     size_t len = strlen(string);
-    CeString *self = malloc(sizeof(CeString) + len - 1);
+    CeString *self = malloc(sizeof(CeString) + len - 1U);
     self->type = CE_STRING;
-    memcpy(&(self->content), string, len + 1);
+    memcpy(&(self->content), string, len + 1U);
     return (Centity *)self;
 }
 
@@ -58,7 +58,7 @@ Centity *
 centity_createSizedList(size_t initialSize)
 {
     CeList *self = malloc(sizeof(CeList) +
-	    (initialSize - 1) * sizeof(Centity *));
+	    (initialSize - 1U) * sizeof(Centity *));
     self->type = CE_LIST;
     memset(&(self->memb), 0, initialSize * sizeof(Centity *));
     return (Centity *)self;
@@ -73,11 +73,11 @@ centity_createList(void)
 Centity *
 centity_createBitsDict(unsigned int hashbits)
 {
-    unsigned int hashmask = (1 << hashbits) - 1;
+    unsigned int hashmask = (1U << hashbits) - 1U;
     CeDict *self = malloc(sizeof(CeList) + hashmask * sizeof(Centity *));
     self->type = CE_DICT;
     self->hashmask = hashmask;
-    memset(&(self->memb), 0, (hashmask + 1) * sizeof(CeDictEntry *));
+    memset(&(self->memb), 0, (hashmask + 1U) * sizeof(CeDictEntry *));
     return (Centity *)self;
 }
 
