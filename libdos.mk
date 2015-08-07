dae912e760bfc39e3db9f5522bed63393e8b02c0
Felix Palmen <felix@palmen-it.de>
2015-08-07 20:42:31 +0200

    first DOS .COM build working in dosbox

--- libdos.mk
+++ libdos.mk
@@ -9,19 +9,33 @@ SOURCES:= src/board.c src/csnake.c src/food.c src/game.c src/screen.c \
 LIBDOS:= $(LDP)/core.c $(LDP)/stdlib.c $(LDP)/string.c $(LDP)/stdio.c \
 	$(LDP)/time.c $(LDP)/conio.c $(LDP)/curses.c $(LDP)/rtctimer.c
 
-CFLAGS:= -std=c99 -Os -nostdlib -m16 -march=i386 -ffreestanding -I$(LDP) \
-	-Wall -Wextra -pedantic -DUSEMSCP -DNOARGV -DDOSREAL
+CFLAGS:= -std=c99 -Oz -nostdlib -m16 -march=i386 -mregparm=3 \
+	-ffreestanding -ffunction-sections -fdata-sections -I$(LDP) \
+	-Wall -Wextra -pedantic -DUSEMSCP -DNOARGV
 
-LDFLAGS:= -Wl,--nmagic,--script=$(LDBP)/com.ld
+LDFLAGS:= -Wl,--omagic,--script=$(LDBP)/com.ld,--gc-sections
 
 ALLSRC:= $(LIBDOS) $(SOURCES)
 
 all: $(BINARY)
 
-$(BINARY): $(ALLSRC) | bin $(LDBP)/com.ld
+$(BINARY): $(ALLSRC:.c=.o) | bin $(LDBP)/com.ld
 	clang -o$@ $(CFLAGS) $(LDFLAGS) $^
+
+%.o: %.c
+
+
+%.o: %.s
+	echo \\t.code16 | cat - $< | as --32 -march=i586 -o$@
+
+%.s: %.c
+	clang -S -o$@ $(CFLAGS) $<
 
 bin:
 	mkdir bin
+
+clean:
+	rm -f $(ALLSRC:.c=.o)
+	rm -f $(ALLSRC:.c=.s)
 
 .PHONY: all $(BINARY)
