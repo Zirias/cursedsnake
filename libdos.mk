35bc6552759af866cf4dbb640ab0ae0fb957ced6
Felix Palmen <felix@palmen-it.de>
2015-08-09 01:22:59 +0200

    pull in new version of clang-libdos

--- libdos.mk
+++ libdos.mk
@@ -1,41 +1,25 @@
-LDBP:= externals/clang-libdos
-LDP:= $(LDBP)/src/libdos
+all: doscom/csnake.com
 
-BINARY:= bin/csnake.com
+LIBDOSPATH:= externals/clang-libdos
 
-SOURCES:= src/board.c src/csnake.c src/food.c src/game.c src/screen.c \
-	src/snake.c src/utils.c src/platform/dosreal/ticker.c
+include $(LIBDOSPATH)/mk/doscom.mk
 
-LIBDOS:= $(LDP)/core.c $(LDP)/stdlib.c $(LDP)/string.c $(LDP)/stdio.c \
-	$(LDP)/time.c $(LDP)/conio.c $(LDP)/curses.c $(LDP)/rtctimer.c
+csnake_TARGET:= csnake.com
+csnake_SRCDIR:= src
+csnake_TGTDIR:= doscom
+csnake_ASMDIR:= doscom/asm
+csnake_OBJDIR:= doscom/obj
+csnake_MODULES:= board csnake food game screen snake utils \
+	platform/dosreal/ticker
+csnake_CFLAGS:= -DUSEMSCP
 
-CFLAGS:= -std=c99 -Oz -nostdlib -m16 -march=i386 -mregparm=3 \
-	-ffreestanding -ffunction-sections -fdata-sections -I$(LDP) \
-	-Wall -Wextra -pedantic -DUSEMSCP -DNOARGV
-
-LDFLAGS:= -Wl,--omagic,--script=$(LDBP)/com.ld,--gc-sections
-
-ALLSRC:= $(LIBDOS) $(SOURCES)
-
-all: $(BINARY)
-
-$(BINARY): $(ALLSRC:.c=.o) | bin $(LDBP)/com.ld
-	clang -o$@ $(CFLAGS) $(LDFLAGS) $^
-
-%.o: %.c
-
-
-%.o: %.s
-	echo \\t.code16 | cat - $< | as --32 -march=i586 -o$@
-
-%.s: %.c
-	clang -S -o$@ $(CFLAGS) $<
-
-bin:
-	mkdir bin
+T:=csnake
+$(eval $(DOSCOMRULES))
 
 clean:
-	rm -f $(ALLSRC:.c=.o)
-	rm -f $(ALLSRC:.c=.s)
+	rm -f $(CLEAN)
 
-.PHONY: all $(BINARY)
+distclean: clean
+	rm -fr doscom
+
+.PHONY: all clean distclean
